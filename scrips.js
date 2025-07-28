e647a9a9a1a5f23f28068eea51a69aae300ebc11
DIELTEAM <malimbavictor2@gmail.com>
2025-07-28 16:40:30 -0500

    mivida

---
+++ scrips.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.nav-tabs a');
+            navLinks.forEach(link => link.classList.remove('active'));
+            this.classList.add('active');
+});
